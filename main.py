94040478ce7841440b8154beddf9f160c57a6a2c
rodrigov5 <rodrigovelosow@gmail.com>
2022-10-01 14:52:41 -0300

    Att

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
 import pandas as pd
-import csv
+import csv, string
 
 
 
 data = pd.read_excel("debate_band.xlsx", usecols='F')
 
+
 palavras = ''
 for i in data["Texto"]:
     palavras = palavras + i
 
+palavras.lower()
 listaPalavras = palavras.split() # separa cada palavra presente no texto em uma lista = ['palavraExemplo', 'palavraExemplo', 'palavraExemplo']
 
-palavrafreqs = [0,0,0]
-for plvs in listaPalavras:
-    if plvs == 'voto':
-        palavrafreqs[0] += 1
-    if plvs == 'candidato':
-        palavrafreqs[1] += 1
-    if plvs == 'governo':
-        palavrafreqs[2] += 1
+
+palavrafreqs = []
+for p in listaPalavras:
+    palavrafreqs.append(listapalavras.count(p))
+
+
+
 
 
 colunas = ['Palavra', 'Frequência']
 
-linhas = [
-    ['Voto', palavrafreqs[0]],
-    ['Candidato', palavrafreqs[1]],
-    ['Governo', palavrafreqs[2]]
-]
+linhas = [str(list(zip(listaPalavras, palavrafreqs)))]
 
 with open('frequencias.csv', 'w', newline="") as f:
     write = csv.writer(f, delimiter=";")
